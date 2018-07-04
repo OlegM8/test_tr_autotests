8fb1a0fe63efec005d7431e4f31139d27eb48abd
Oleg Mikheenko <olegmikheenko@Olegs-MacBook-Pro.local>
2018-07-04 16:41:00 +0300

    webdriver changed to docker

--- fixture/application.py
+++ fixture/application.py
@@ -3,10 +3,22 @@ from fixture.session import SessionHelper
 from fixture.calculator import CalcHelper
 
 
+capabilities = {
+    "browserName": "firefox",
+    "version": "58.0",
+    "enableVNC": True
+}
+
+driver = webdriver.Remote(
+    command_executor="http://localhost:4444/wd/hub",
+    desired_capabilities=capabilities,
+    )
+
+
 class Application:
 
     def __init__(self):
-        self.wd = webdriver.Chrome()
+        self.wd = driver
         self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.calculator = CalcHelper(self)
